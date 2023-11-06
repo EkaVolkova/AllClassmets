291de1b7cd005f534b3a6635591dee2440b138fe
Ekaterina Volkova <koweevaea@gmail.com>
2023-11-06 20:22:26 +0500

    Есть список учеников школы с разбивкой по классам. Напишите метод, который соберет всех учеников всех классов в один список, используя LINQ

---
+++ AllClassmets/AllClassmets/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// Есть список учеников школы с разбивкой по классам:
+/// Напишите метод, который соберет всех учеников всех классов в один список, используя LINQ.
+/// </summary>
+namespace AllClassmets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            return classes.SelectMany(c => c.Students).ToArray();
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
